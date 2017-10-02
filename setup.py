448e82dc7187d522c15d0c549af17d0440c18506
Reto Stauffer <reto.stauffer@uibk.ac.at>
2017-10-02 15:25:40 +0200

    Version set to 1.0-0 BETA

--- setup.py
+++ setup.py
@@ -7,17 +7,17 @@
 # -------------------------------------------------------------------
 # - EDITORIAL:   2017-02-05, RS: Created file on thinkreto.
 # -------------------------------------------------------------------
-# - L@ST MODIFIED: 2017-08-20 10:54 on pc24-c707
+# - L@ST MODIFIED: 2017-10-02 15:25 on thinkreto
 # -------------------------------------------------------------------
 from setuptools import setup
 
 setup(name='GFSV2',     # This is the package name
-      version='0.1-3',            # Current package version, what else
+      version='1.0-0',            # Current package version, what else
       description='GFS reforecast version 2 downloader',
       long_description='No long description necessary',
       classifiers=[
-        'Development Status :: 3 - Alpha',
-        #'Development Status :: 4 - Beta',
+        #'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Closed source',
         'Programming Language :: Python :: 2.7',
       ],
